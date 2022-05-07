4a1ea1d229d411ed2c2201687c6892bc92d202cd
barcot <barkotnuri@gmail.com>
2022-05-07 16:15:22 -0500

    test completed task

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -2,22 +2,30 @@ describe('tests to do app', () => {
 
  beforeEach( () => {
    cy.visit('https://todomvc.com/examples/react/#/active')
- })
-
- it('adds and complete a task', () => {
-  cy.get('.new-todo')
+   cy.get('.new-todo')
     .type('learn HTML')
     .type('{enter}')
     .type('Cypress')
     .type('{enter}')
     .type('learn JS')
     .type('{enter}')
+ })
+
+ it('adds and complete a task', () => {
 
   cy.contains('learn HTML').prev().check()
   cy.contains('learn HTML').should('not.exist')
 
   cy.contains('Completed').click()
   cy.contains('learn HTML').should('exist')
+
+  })
+  it('removes completed tasks', () => {
+  cy.contains('learn HTML').prev().check()
+  cy.contains('Clear completed').click()
+  cy.contains('Completed').click()
+  cy.contains('learn HTML').should('not.exist')
+
 
   })
 })
